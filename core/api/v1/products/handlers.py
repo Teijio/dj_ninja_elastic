c4db1cfe90ca2bd5318f940bbfd7cd13712fdf4e
arthur <ridpfrep@yandex.ru>
2024-11-18 21:37:24 +0300

    DDD updated

--- core/api/v1/products/handlers.py
+++ core/api/v1/products/handlers.py
@@ -2,9 +2,12 @@ from django.http import HttpRequest
 from ninja import Router
 
 from core.api.v1.products.schemas import ProductListSchema
+from core.apps.products.services.products import BaseProductService, ORMProductService
 
 router = Router(tags=["Products"])
 
 @router.get("", response=ProductListSchema)
 def get_product_list_handler(request: HttpRequest) -> ProductListSchema:
-    return []
+    service: BaseProductService = ORMProductService()
+    product_list = service.get_product_list()
+    return product_list
