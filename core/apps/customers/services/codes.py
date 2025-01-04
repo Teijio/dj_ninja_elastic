0a40a32b26bb31960a65119f65f0660e466aca35
arthur <ridpfrep@yandex.ru>
2025-01-04 14:38:50 +0300

    exceptions added

--- core/apps/customers/services/codes.py
+++ core/apps/customers/services/codes.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+import random
+
+from django.core.cache import cache
 
 from core.apps.customers.entities import CustomerEntity
 
@@ -9,3 +12,14 @@ class BaseCodeService(ABC):
 
     @abstractmethod
     def validate_code(self, code: str, customer: CustomerEntity) -> None: ...
+
+
+class DjangoCacheCodeService(BaseCodeService):
+    def generate_code(self, customer: CustomerEntity) -> str:
+        code = str(random.randint(100000, 999999))
+        cache.set(customer.phone, code)
+        return code
+
+    def validate_code(self, code: str, customer: CustomerEntity) -> None:
+        code = cache.get(customer.phone, code)
+        
--- core/apps/customers/services/customers.py
+++ core/apps/customers/services/customers.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
+from uuid import uuid4
 
+from core.apps.customers.models import Customer as CustomerModel
 from core.apps.customers.entities import CustomerEntity
 
 
@@ -12,3 +14,18 @@ class BaseCustomerService(ABC):
 
     @abstractmethod
     def get(self, phone: str) -> CustomerEntity: ...
+
+
+class ORMCustomerService(BaseCustomerService):
+    def get_or_create(self, phone) -> CustomerEntity:
+        user_dto, _ = CustomerModel.objects.get_or_create(phone=phone)
+        return user_dto.to_entity()
+
+    def get(self, phone: str) -> CustomerEntity:
+        user_dto = CustomerModel.objects.get(phone=phone)
+        return user_dto.to_entity()
+
+    def generate_token(self, customer: CustomerEntity) -> str:
+        new_token = str(uuid4())
+        CustomerModel.objects.filter(phone=customer.phone).update(token=new_token)
+        return new_token
